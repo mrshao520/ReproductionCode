139ad6a206798c7de35a95c8658c996a47067e73
华羽轩 <shao18955760662@outlook.com>
2024-03-09 16:18:01 +0800

    SRCNN_mytorch

---
+++ Super-Resolution_CNN/SRCNN_mytorch/model.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SRCNN915(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.relu(self.conv2(out))
+        out = self.conv3(out)
+        
+        return out
+    
+class SRCNN955(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.relu(self.conv2(out))
+        out = self.conv3(out)
+        
+        return out
+    
+class SRCNN935(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.relu(self.conv2(out))
+        out = self.conv3(out)
+        
+        return out
